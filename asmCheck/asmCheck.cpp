5494945d5896104fedd38458b0f94a4c74b582ee
komo97 <jose.m.perez@saei.mx>
2017-05-02 07:51:55 -0600

    asmcheck
    # Conflicts:
    #	.vs/pedroOOP/v15/Browse.VC.db
    #	.vs/pedroOOP/v15/Solution.VC.db-shm
    #	.vs/pedroOOP/v15/Solution.VC.db-wal

---
+++ asmCheck/asmCheck.cpp
@@ -0,0 +1,25 @@
+// asmCheck.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int mahNigga() {
+	return 22;
+}
+
+int mahBoi() {
+	return 33;
+}
+
+bool checkerino(int a, int b)
+{
+	return a < b;
+}
+
+int main()
+{
+	int wut = checkerino(0,1) ? mahNigga() : mahBoi() + 33;
+	wut += 33;
+	return 0;
+}
+
